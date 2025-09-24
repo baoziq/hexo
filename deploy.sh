866961fd84df61fedbd3b5bba60ee3a2a25fecc1
qiushaojie <qiushaojie@aspirecn.com>
2025-09-24 15:13:47 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,19 @@
 
 set -e
 
+echo "git pull"
 git pull
+echo "git add ."
 git add .
 commit_msg=${1:-"update: $(date '+%Y-%m-%d %H:%M:%S')"}
+echo "git commit"
 git commit -m "$commit_msg"
+echo "git push"
 git push origin main
 
+echo "hexo clean"
 hexo clean
+echo "hexo g -d"
 hexo g -d
+echo "finish"
 
